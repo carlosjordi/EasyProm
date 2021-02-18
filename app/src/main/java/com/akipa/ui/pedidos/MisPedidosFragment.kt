f515e6141c7db60735c0d048102710f8e1c6277b
Juan Carlos Castillo Aycachi <carlosjordi@hotmail.com>
2021-02-18 02:44:27 -0500

    Anadida navegacion hacia el detalle del pedido

--- app/src/main/java/com/akipa/ui/pedidos/MisPedidosFragment.kt
+++ app/src/main/java/com/akipa/ui/pedidos/MisPedidosFragment.kt
@@ -7,7 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.akipa.databinding.FragmentMisPedidosBinding
+import com.akipa.entidades.toCabeceraPedido
 
 class MisPedidosFragment : Fragment() {
 
@@ -24,7 +26,11 @@ class MisPedidosFragment : Fragment() {
         binding.viewModel = viewModel
 
         val adapter = MisPedidosAdapter(PedidosListener {
-            // cuando hagamos click
+            findNavController().navigate(
+                MisPedidosFragmentDirections.actionMisPedidosFragmentToDetalleMisPedidosFragment(
+                    it.toCabeceraPedido()
+                )
+            )
         })
         binding.listaMisPedidos.adapter = adapter
 
